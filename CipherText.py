5c4b2f06ea3ab792231aeb94a23fca9360646215
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-04-20 12:17:50 +0800

    pseudocode

---
+++ CipherText.py
@@ -0,0 +1,12 @@
+# create function
+# convert inputs to uppercase
+# convert inputs to corresponding letter values (0-25)
+# repeat key to match length of message
+# add both numbers
+# convert encrypted numbers to corresponding letters (A-Z)
+# return/print value
+
+# ask for input, then save
+# call the function
+
+# create design
