5343664f032402266fc9c6f5b1c14c07e1240bec
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-04-20 12:22:18 +0800

    setting up the function

--- CipherText.py
+++ CipherText.py
@@ -1,10 +1,20 @@
 # create function
+def encrypt_letters(message, key):
 # convert inputs to uppercase
+    message = message.upper()
+    key = key.upper()
 # convert inputs to corresponding letter values (0-25)
+    message_number = [ord(letter) - 65 for letter in message if message.isupper()]
+    key_number = [ord(letter) - 65 for letter in key if key.isupper()]
 # repeat key to match length of message
+    key_number = key_number * (len(message_number) // len(key_number) + 1)
+    key_number = key_number [:len(message_number)]
 # add both numbers
+    encrypted_numbers = [(m + k) % 26 for m, k in zip(message_number, key_number)]
 # convert encrypted numbers to corresponding letters (A-Z)
+    ciphertext = ''.join([chr(value + 65) for value in encrypted_numbers])
 # return/print value
+    return ("Your ciphertext is: " + ciphertext)
 
 # ask for input, then save
 # call the function
